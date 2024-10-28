256ac4787713b77212c288056db5fd3984a3b533
Ricardo Lopes <Ric22031999@gmail.com>
2024-10-27 22:09:57 -0300

    Calculadora

---
+++ WinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,465 @@
+﻿namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Button botaoTAN;
+            botaoDividir = new Button();
+            botaoVezes = new Button();
+            botaoMais = new Button();
+            botaoMenos = new Button();
+            igual = new Button();
+            Virgula = new Button();
+            botao0 = new Button();
+            botao3 = new Button();
+            botao2 = new Button();
+            botao1 = new Button();
+            botao6 = new Button();
+            botao5 = new Button();
+            botao4 = new Button();
+            botao9 = new Button();
+            botao8 = new Button();
+            botao7 = new Button();
+            richBox1 = new RichTextBox();
+            Lbl1 = new Label();
+            botaoC = new Button();
+            button27 = new Button();
+            botaoCE = new Button();
+            button22 = new Button();
+            botaoMS = new Button();
+            botaoSIN = new Button();
+            botaoCOS = new Button();
+            botaoPotencia = new Button();
+            botaoRadiciacao = new Button();
+            botaoPotencia2 = new Button();
+            RaizQuadrada = new Button();
+            log = new Button();
+            botaoTAN = new Button();
+            SuspendLayout();
+            // 
+            // botaoTAN
+            // 
+            botaoTAN.BackColor = SystemColors.ActiveBorder;
+            botaoTAN.Location = new Point(467, 231);
+            botaoTAN.Name = "botaoTAN";
+            botaoTAN.Size = new Size(68, 58);
+            botaoTAN.TabIndex = 40;
+            botaoTAN.Text = "TAN";
+            botaoTAN.UseVisualStyleBackColor = false;
+            botaoTAN.Click += botaoTAN_Click_1;
+            // 
+            // botaoDividir
+            // 
+            botaoDividir.BackColor = SystemColors.ActiveBorder;
+            botaoDividir.Location = new Point(241, 103);
+            botaoDividir.Name = "botaoDividir";
+            botaoDividir.Size = new Size(68, 58);
+            botaoDividir.TabIndex = 33;
+            botaoDividir.Text = "/";
+            botaoDividir.UseVisualStyleBackColor = false;
+            botaoDividir.Click += botaoDividir_Click;
+            // 
+            // botaoVezes
+            // 
+            botaoVezes.BackColor = SystemColors.ActiveBorder;
+            botaoVezes.Location = new Point(241, 167);
+            botaoVezes.Name = "botaoVezes";
+            botaoVezes.Size = new Size(68, 58);
+            botaoVezes.TabIndex = 32;
+            botaoVezes.Text = "X";
+            botaoVezes.UseVisualStyleBackColor = false;
+            botaoVezes.Click += botaoVezes_Click;
+            // 
+            // botaoMais
+            // 
+            botaoMais.BackColor = SystemColors.ActiveBorder;
+            botaoMais.Location = new Point(241, 231);
+            botaoMais.Name = "botaoMais";
+            botaoMais.Size = new Size(68, 58);
+            botaoMais.TabIndex = 31;
+            botaoMais.Text = "+";
+            botaoMais.UseVisualStyleBackColor = false;
+            botaoMais.Click += botaoMais_Click;
+            // 
+            // botaoMenos
+            // 
+            botaoMenos.BackColor = SystemColors.ActiveBorder;
+            botaoMenos.Location = new Point(241, 295);
+            botaoMenos.Name = "botaoMenos";
+            botaoMenos.Size = new Size(68, 58);
+            botaoMenos.TabIndex = 30;
+            botaoMenos.Text = "-";
+            botaoMenos.UseVisualStyleBackColor = false;
+            botaoMenos.Click += botaoMenos_Click;
+            // 
+            // igual
+            // 
+            igual.BackColor = SystemColors.ActiveCaption;
+            igual.Location = new Point(167, 295);
+            igual.Name = "igual";
+            igual.Size = new Size(68, 58);
+            igual.TabIndex = 29;
+            igual.Text = "=";
+            igual.UseVisualStyleBackColor = false;
+            igual.Click += igual_Click;
+            // 
+            // Virgula
+            // 
+            Virgula.Location = new Point(93, 295);
+            Virgula.Name = "Virgula";
+            Virgula.Size = new Size(68, 58);
+            Virgula.TabIndex = 28;
+            Virgula.Text = ".";
+            Virgula.UseVisualStyleBackColor = true;
+            Virgula.Click += Virgula_Click;
+            // 
+            // botao0
+            // 
+            botao0.Location = new Point(19, 295);
+            botao0.Name = "botao0";
+            botao0.Size = new Size(68, 58);
+            botao0.TabIndex = 27;
+            botao0.Text = "0";
+            botao0.UseVisualStyleBackColor = true;
+            botao0.Click += botao0_Click;
+            // 
+            // botao3
+            // 
+            botao3.Location = new Point(167, 231);
+            botao3.Name = "botao3";
+            botao3.Size = new Size(68, 58);
+            botao3.TabIndex = 26;
+            botao3.Text = "3";
+            botao3.UseVisualStyleBackColor = true;
+            botao3.Click += botao3_Click;
+            // 
+            // botao2
+            // 
+            botao2.Location = new Point(93, 231);
+            botao2.Name = "botao2";
+            botao2.Size = new Size(68, 58);
+            botao2.TabIndex = 25;
+            botao2.Text = "2";
+            botao2.UseVisualStyleBackColor = true;
+            botao2.Click += botao2_Click;
+            // 
+            // botao1
+            // 
+            botao1.Location = new Point(19, 231);
+            botao1.Name = "botao1";
+            botao1.Size = new Size(68, 58);
+            botao1.TabIndex = 24;
+            botao1.Text = "1";
+            botao1.UseVisualStyleBackColor = true;
+            botao1.Click += botao1_Click;
+            // 
+            // botao6
+            // 
+            botao6.Location = new Point(167, 167);
+            botao6.Name = "botao6";
+            botao6.Size = new Size(68, 58);
+            botao6.TabIndex = 23;
+            botao6.Text = "6";
+            botao6.UseVisualStyleBackColor = true;
+            botao6.Click += botao6_Click;
+            // 
+            // botao5
+            // 
+            botao5.Location = new Point(93, 167);
+            botao5.Name = "botao5";
+            botao5.Size = new Size(68, 58);
+            botao5.TabIndex = 22;
+            botao5.Text = "5";
+            botao5.UseVisualStyleBackColor = true;
+            botao5.Click += botao5_Click;
+            // 
+            // botao4
+            // 
+            botao4.Location = new Point(19, 167);
+            botao4.Name = "botao4";
+            botao4.Size = new Size(68, 58);
+            botao4.TabIndex = 21;
+            botao4.Text = "4";
+            botao4.UseVisualStyleBackColor = true;
+            botao4.Click += botao4_Click;
+            // 
+            // botao9
+            // 
+            botao9.Location = new Point(167, 103);
+            botao9.Name = "botao9";
+            botao9.Size = new Size(68, 58);
+            botao9.TabIndex = 20;
+            botao9.Text = "9";
+            botao9.UseVisualStyleBackColor = true;
+            botao9.Click += botao9_Click;
+            // 
+            // botao8
+            // 
+            botao8.Location = new Point(93, 103);
+            botao8.Name = "botao8";
+            botao8.Size = new Size(68, 58);
+            botao8.TabIndex = 19;
+            botao8.Text = "8";
+            botao8.UseVisualStyleBackColor = true;
+            botao8.Click += botao8_Click;
+            // 
+            // botao7
+            // 
+            botao7.Location = new Point(19, 103);
+            botao7.Name = "botao7";
+            botao7.Size = new Size(68, 58);
+            botao7.TabIndex = 18;
+            botao7.Text = "7";
+            botao7.UseVisualStyleBackColor = true;
+            botao7.Click += botao7_Click;
+            // 
+            // richBox1
+            // 
+            richBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            richBox1.Location = new Point(19, 12);
+            richBox1.Name = "richBox1";
+            richBox1.RightToLeft = RightToLeft.Yes;
+            richBox1.Size = new Size(776, 74);
+            richBox1.TabIndex = 17;
+            richBox1.Text = "";
+            // 
+            // Lbl1
+            // 
+            Lbl1.AutoSize = true;
+            Lbl1.Location = new Point(32, 34);
+            Lbl1.Name = "Lbl1";
+            Lbl1.Size = new Size(0, 20);
+            Lbl1.TabIndex = 34;
+            // 
+            // botaoC
+            // 
+            botaoC.BackColor = SystemColors.ActiveBorder;
+            botaoC.Location = new Point(319, 103);
+            botaoC.Name = "botaoC";
+            botaoC.Size = new Size(68, 58);
+            botaoC.TabIndex = 35;
+            botaoC.Text = "C";
+            botaoC.UseVisualStyleBackColor = false;
+            botaoC.Click += botaoC_Click;
+            // 
+            // button27
+            // 
+            button27.BackColor = SystemColors.ActiveBorder;
+            button27.Location = new Point(541, 167);
+            button27.Name = "button27";
+            button27.Size = new Size(68, 58);
+            button27.TabIndex = 46;
+            button27.Text = "MC";
+            button27.UseVisualStyleBackColor = false;
+            // 
+            // botaoCE
+            // 
+            botaoCE.BackColor = SystemColors.ActiveBorder;
+            botaoCE.Location = new Point(393, 103);
+            botaoCE.Name = "botaoCE";
+            botaoCE.Size = new Size(68, 58);
+            botaoCE.TabIndex = 45;
+            botaoCE.Text = "CE";
+            botaoCE.UseVisualStyleBackColor = false;
+            botaoCE.Click += botaoCE_Click;
+            // 
+            // button22
+            // 
+            button22.BackColor = SystemColors.ActiveBorder;
+            button22.Location = new Point(541, 103);
+            button22.Name = "button22";
+            button22.Size = new Size(68, 58);
+            button22.TabIndex = 44;
+            button22.Text = "MR";
+            button22.UseVisualStyleBackColor = false;
+            // 
+            // botaoMS
+            // 
+            botaoMS.BackColor = SystemColors.ActiveBorder;
+            botaoMS.Location = new Point(541, 231);
+            botaoMS.Name = "botaoMS";
+            botaoMS.Size = new Size(68, 58);
+            botaoMS.TabIndex = 43;
+            botaoMS.Text = "MS";
+            botaoMS.UseVisualStyleBackColor = false;
+            botaoMS.Click += botaoMS_Click;
+            // 
+            // botaoSIN
+            // 
+            botaoSIN.BackColor = SystemColors.ActiveBorder;
+            botaoSIN.Location = new Point(467, 103);
+            botaoSIN.Name = "botaoSIN";
+            botaoSIN.Size = new Size(68, 58);
+            botaoSIN.TabIndex = 42;
+            botaoSIN.Text = "SIN";
+            botaoSIN.UseVisualStyleBackColor = false;
+            botaoSIN.Click += botaoSIN_Click;
+            // 
+            // botaoCOS
+            // 
+            botaoCOS.BackColor = SystemColors.ActiveBorder;
+            botaoCOS.Location = new Point(467, 167);
+            botaoCOS.Name = "botaoCOS";
+            botaoCOS.Size = new Size(68, 58);
+            botaoCOS.TabIndex = 41;
+            botaoCOS.Text = "COS";
+            botaoCOS.UseVisualStyleBackColor = false;
+            botaoCOS.Click += botaoCOS_Click;
+            // 
+            // botaoPotencia
+            // 
+            botaoPotencia.BackColor = SystemColors.ActiveBorder;
+            botaoPotencia.Location = new Point(319, 167);
+            botaoPotencia.Name = "botaoPotencia";
+            botaoPotencia.Size = new Size(68, 58);
+            botaoPotencia.TabIndex = 39;
+            botaoPotencia.Text = "x^y";
+            botaoPotencia.UseVisualStyleBackColor = false;
+            botaoPotencia.Click += botaoPotencia_Click;
+            // 
+            // botaoRadiciacao
+            // 
+            botaoRadiciacao.BackColor = SystemColors.ActiveBorder;
+            botaoRadiciacao.Location = new Point(319, 231);
+            botaoRadiciacao.Name = "botaoRadiciacao";
+            botaoRadiciacao.Size = new Size(68, 58);
+            botaoRadiciacao.TabIndex = 38;
+            botaoRadiciacao.Text = "Y √ X";
+            botaoRadiciacao.UseVisualStyleBackColor = false;
+            botaoRadiciacao.Click += botaoRadiciacao_Click;
+            // 
+            // botaoPotencia2
+            // 
+            botaoPotencia2.BackColor = SystemColors.ActiveBorder;
+            botaoPotencia2.Location = new Point(319, 295);
+            botaoPotencia2.Name = "botaoPotencia2";
+            botaoPotencia2.Size = new Size(68, 58);
+            botaoPotencia2.TabIndex = 37;
+            botaoPotencia2.Text = "X²";
+            botaoPotencia2.UseVisualStyleBackColor = false;
+            botaoPotencia2.Click += botaoPotencia2_Click;
+            // 
+            // RaizQuadrada
+            // 
+            RaizQuadrada.BackColor = SystemColors.ActiveBorder;
+            RaizQuadrada.Location = new Point(393, 167);
+            RaizQuadrada.Name = "RaizQuadrada";
+            RaizQuadrada.Size = new Size(68, 58);
+            RaizQuadrada.TabIndex = 36;
+            RaizQuadrada.Text = "²√ X";
+            RaizQuadrada.UseVisualStyleBackColor = false;
+            RaizQuadrada.Click += RaizQuadrada_Click;
+            // 
+            // log
+            // 
+            log.BackColor = SystemColors.ActiveBorder;
+            log.Location = new Point(393, 231);
+            log.Name = "log";
+            log.Size = new Size(68, 58);
+            log.TabIndex = 47;
+            log.Text = "LOG";
+            log.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(log);
+            Controls.Add(button27);
+            Controls.Add(botaoCE);
+            Controls.Add(button22);
+            Controls.Add(botaoMS);
+            Controls.Add(botaoSIN);
+            Controls.Add(botaoCOS);
+            Controls.Add(botaoTAN);
+            Controls.Add(botaoPotencia);
+            Controls.Add(botaoRadiciacao);
+            Controls.Add(botaoPotencia2);
+            Controls.Add(RaizQuadrada);
+            Controls.Add(botaoC);
+            Controls.Add(Lbl1);
+            Controls.Add(botaoDividir);
+            Controls.Add(botaoVezes);
+            Controls.Add(botaoMais);
+            Controls.Add(botaoMenos);
+            Controls.Add(igual);
+            Controls.Add(Virgula);
+            Controls.Add(botao0);
+            Controls.Add(botao3);
+            Controls.Add(botao2);
+            Controls.Add(botao1);
+            Controls.Add(botao6);
+            Controls.Add(botao5);
+            Controls.Add(botao4);
+            Controls.Add(botao9);
+            Controls.Add(botao8);
+            Controls.Add(botao7);
+            Controls.Add(richBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button botaoDividir;
+        private Button botaoVezes;
+        private Button botaoMais;
+        private Button botaoMenos;
+        private Button igual;
+        private Button Virgula;
+        private Button botao0;
+        private Button botao3;
+        private Button botao2;
+        private Button botao1;
+        private Button botao6;
+        private Button botao5;
+        private Button botao4;
+        private Button botao9;
+        private Button botao8;
+        private Button botao7;
+        private RichTextBox richBox1;
+        private Label Lbl1;
+        private Button botaoC;
+        private Button button27;
+        private Button botaoCE;
+        private Button button22;
+        private Button botaoMS;
+        private Button botaoSIN;
+        private Button botaoCOS;
+        private Button botaoTAN;
+        private Button button20;
+        private Button botaoRadiciacao;
+        private Button botaoPotencia2;
+        private Button RaizQuadrada;
+        private Button log;
+        private Button botaoPotencia;
+    }
+}
---
+++ WinFormsApp3/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System.Linq.Expressions;
+
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public decimal Resultado { get; set; }
+        public decimal Valor { get; set; }
+        private Operacao OperacaoSelecionada { get; set; }
+        private enum Operacao
+        {
+            Adicao,
+            Subtracao,
+            Multiplicacao,
+            Divisao,
+            Potenciacao,
+            Radiciacao,
+            Potencia2,
+            RaizQuadrada,
+            Seno,
+            Cosseno,
+            Tangente
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        
+
+        private void botao0_Click(object sender, EventArgs e)
+        {
+            richBox1.Text += "0";
+
+        }
+
+        private void botao1_Click(object sender, EventArgs e)
+        {
+            richBox1.Text += "1";
+        }
+
+        private void botao2_Click(object sender, EventArgs e)
+        {
+            richBox1.Text += "2";
+
+        }
+
+        private void botao3_Click(object sender, EventArgs e)
+        {
+            richBox1.Text += "3";
+
+        }
+
+        private void botao4_Click(object sender, EventArgs e)
+        {
+            richBox1.Text += "4";
+
+        }
+
+        private void botao5_Click(object sender, EventArgs e)
+        {
+            richBox1.Text += "5";
+
+        }
+
+        private void botao6_Click(object sender, EventArgs e)
+        {
+            richBox1.Text += "6";
+
+        }
+
+        private void botao7_Click(object sender, EventArgs e)
+        {
+            richBox1.Text += "7";
+
+        }
+
+        private void botao8_Click(object sender, EventArgs e)
+        {
+            richBox1.Text += "8";
+
+        }
+
+        private void botao9_Click(object sender, EventArgs e)
+        {
+            richBox1.Text += "9";
+
+        }
+
+        private void botaoMenos_Click(object sender, EventArgs e)
+        {
+            OperacaoSelecionada = Operacao.Subtracao;
+            Valor = Convert.ToDecimal(richBox1.Text);
+            richBox1.Text = "";
+            Lbl1.Text = "-";
+
+        }
+
+        private void botaoMais_Click(object sender, EventArgs e)
+        {
+            OperacaoSelecionada = Operacao.Adicao;
+            Valor = Convert.ToDecimal(richBox1.Text);
+            richBox1.Text = "";
+            Lbl1.Text = "+";
+
+        }
+
+        private void botaoVezes_Click(object sender, EventArgs e)
+        {
+            OperacaoSelecionada = Operacao.Multiplicacao;
+            Valor = Convert.ToDecimal(richBox1.Text);
+            richBox1.Text = "";
+            Lbl1.Text = "*";
+        }
+
+        private void botaoDividir_Click(object sender, EventArgs e)
+        {
+            OperacaoSelecionada = Operacao.Divisao;
+            Valor = Convert.ToDecimal(richBox1.Text);
+            richBox1.Text = "";
+            Lbl1.Text = "/";
+        }
+
+        private void igual_Click(object sender, EventArgs e)
+        {
+            decimal novoValor = 0;
+            if (richBox1.Text != "")
+            {
+                novoValor = Convert.ToDecimal(richBox1.Text);
+            }
+
+            switch (OperacaoSelecionada)
+            {
+                case Operacao.Adicao:
+                    Resultado = Valor + novoValor;
+                    break;
+                case Operacao.Subtracao:
+                    Resultado = Valor - novoValor;
+                    break;
+                case Operacao.Multiplicacao:
+                    Resultado = Valor * novoValor;
+                    break;
+                case Operacao.Divisao:
+                    Resultado = Valor / novoValor;
+                    break;
+                case Operacao.Potenciacao:
+                    Resultado = (decimal)Math.Pow((double)Valor, (double)novoValor);
+                    break;
+                case Operacao.Radiciacao:
+                    Resultado = (decimal)Math.Pow((double)Valor, 1.0 / (double)novoValor);
+                    break;
+                case Operacao.Potencia2:
+                    Resultado = Valor * Valor;
+                    break;
+                case Operacao.RaizQuadrada:
+                    Resultado = (decimal)Math.Sqrt((double)Valor);
+                    break;
+                case Operacao.Seno:
+                    Resultado = (decimal)Math.Sin((double)Valor * Math.PI / 180.0);
+                    break;
+                case Operacao.Cosseno:
+                    Resultado = (decimal)Math.Cos((double)Valor * Math.PI / 180.0);
+                    break;
+                case Operacao.Tangente:
+                    Resultado = (decimal)Math.Tan((double)Valor * Math.PI / 180.0);
+                    break;
+
+            }
+            richBox1.Text = Convert.ToString(Resultado);
+            Lbl1.Text = "=";
+        }
+
+        private void Virgula_Click(object sender, EventArgs e)
+        {
+            if (!richBox1.Text.Contains(","))
+                richBox1.Text += ",";
+        }
+
+        private void botaoC_Click(object sender, EventArgs e)
+        {
+            richBox1.Text = "";
+            Lbl1.Text = "";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botaoCE_Click(object sender, EventArgs e)
+        {
+            if (richBox1.Text.Length > 0)
+            {
+                richBox1.Text = richBox1.Text.Remove(richBox1.Text.Length - 1);
+            }
+        }
+
+        private void botaoPotencia_Click(object sender, EventArgs e)
+        {
+            OperacaoSelecionada = Operacao.Potenciacao;
+            Valor = Convert.ToDecimal(richBox1.Text);
+            richBox1.Text = "";
+            Lbl1.Text = "^";
+        }
+
+        private void botaoRadiciacao_Click(object sender, EventArgs e)
+        {
+            OperacaoSelecionada = Operacao.Radiciacao;
+            Valor = Convert.ToDecimal(richBox1.Text);
+            richBox1.Text = "";
+            Lbl1.Text = "y√x";
+        }
+
+        private void botaoPotencia2_Click(object sender, EventArgs e)
+        {
+            OperacaoSelecionada = Operacao.Potencia2;
+            decimal valor = Convert.ToDecimal(richBox1.Text);
+            Resultado = valor * valor;
+            richBox1.Text = Convert.ToString(Resultado);
+            Lbl1.Text = "x²";
+        }
+
+        private void RaizQuadrada_Click(object sender, EventArgs e)
+        {
+            Valor = Convert.ToDecimal(richBox1.Text);
+            Resultado = (decimal)Math.Sqrt((double)Valor);
+            richBox1.Text = Convert.ToString(Resultado);
+            Lbl1.Text = "²√x";
+        }
+
+        private void botaoSIN_Click(object sender, EventArgs e)
+        {
+            OperacaoSelecionada = Operacao.Seno;
+            Valor = Convert.ToDecimal(richBox1.Text);
+
+            double radianos = (double)Valor * Math.PI / 180.0;
+
+            switch (OperacaoSelecionada)
+            {
+                case Operacao.Seno:
+                    Resultado = (decimal)Math.Sin(radianos);
+                    break;
+            }
+
+            richBox1.Text = Convert.ToString(Resultado);
+            Lbl1.Text = "=";
+        }
+
+        private void botaoCOS_Click(object sender, EventArgs e)
+        {
+            OperacaoSelecionada = Operacao.Cosseno;
+            Valor = Convert.ToDecimal(richBox1.Text);
+
+            switch (OperacaoSelecionada)
+            {
+                case Operacao.Cosseno:
+                    Resultado = (decimal)Math.Cos((double)Valor * Math.PI / 180.0);
+                    break;
+            }
+
+            richBox1.Text = Convert.ToString(Resultado);
+            Lbl1.Text = "=";
+        }
+
+       
+
+        private void botaoMS_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void botaoTAN_Click_1(object sender, EventArgs e)
+        {
+            OperacaoSelecionada = Operacao.Tangente;
+            Valor = Convert.ToDecimal(richBox1.Text);
+
+            switch (OperacaoSelecionada)
+            {
+                case Operacao.Tangente:
+                    Resultado = (decimal)Math.Tan((double)Valor * Math.PI / 180.0);
+                    break;
+            }
+
+            richBox1.Text = Convert.ToString(Resultado);
+            Lbl1.Text = "=";
+        }
+    }
+
+
+}
